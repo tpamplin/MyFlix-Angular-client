ddab5022d7786cc026168a4a89872876896ce0d3
tpamplin <gartiman1@gmail.com>
2025-01-30 10:55:50 -0500

    add some api calls

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -12,6 +12,96 @@ const apiUrl = "https://myflix-timpamplin-021f285e4632.herokuapp.com/"
 
 export class UserRegistrationService {
     constructor(private http: HttpClient){}
+
+    private getStoredToken(): any {
+        return localStorage.getItem('token');
+    }
+
+    private getUserData(): any {
+        return localStorage.getItem('user')
+    }
+
+    public getAllMovies(): Observable<any> {
+        const token = this.getStoredToken();
+        return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+            {
+                Authorization: 'Bearer ' + token,
+            }
+        )}).pipe(
+            map(this.extractResponseData),
+            catchError(this.handleError)
+        )
+
+    }
+
+    public getMovie(title: string): Observable<any> {
+        const token = this.getStoredToken();
+        return this.http.get(apiUrl + 'movies/' + title, {
+            headers: new HttpHeaders(
+            {
+                Authorization: 'Bearer ' + token,
+            }
+            )
+        }).pipe(
+            map(this.extractResponseData),
+            catchError(this.handleError)
+        )
+    }
+
+    public getDirector(name: string): Observable<any> {
+        const token = this.getStoredToken();
+        return this.http.get(apiUrl + "directors/" + name, {
+            headers: new HttpHeaders(
+            {
+                Authorization: 'Bearer ' + token,
+            }
+            )
+        }).pipe(
+            map(this.extractResponseData),
+            catchError(this.handleError)
+        )
+    }
+
+    public getGenre(name: string): Observable<any> {
+        const token = this.getStoredToken();
+        return this.http.get(apiUrl + "genres/" + name, {
+            headers: new HttpHeaders(
+            {
+                Authorization: 'Bearer ' + token,
+            }
+            )
+        }).pipe(
+            map(this.extractResponseData),
+            catchError(this.handleError)
+        )
+    }
+
+    public getUser(): Observable<any> {
+
+    }
+
+    public getUserFavorites(): Observable<any> {
+
+    }
+
+    public addFavorite(): Observable<any> {
+
+    }
+
+    public updateUser(): Observable<any> {
+
+    }
+
+    public deleteUser(): Observable<any> {
+
+    }
+
+    public deleteFavorite(): Observable<any> {
+
+    }
+
+
+
     public userRegistration(userDetails:any): Observable<any> {
         console.log(userDetails);
         return this.http.post(apiUrl + "users", userDetails).pipe(
@@ -26,6 +116,11 @@ export class UserRegistrationService {
             console.error(`Error Status code ${error.status}, error body is: ${error.error}` );
             return throwError("Something bad happened; Please try again later.")
         }
+    }
+
+    private extractResponseData(res: any): any {
+        const body = res;
+        return body || {};
     }
 }
 export class FetchApiDataService {
