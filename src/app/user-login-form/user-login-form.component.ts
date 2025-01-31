4baf06148917f075815111d0ffa8890140b6f579
tpamplin <gartiman1@gmail.com>
2025-01-31 00:57:04 -0500

    create logic and template for user-login-form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+
+import { MatDialogRef } from "@angular/material/dialog";
+
+import { FetchApiDataService } from "../fetch-api-data.service";
+
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-user-login-form',
-  standalone: false,
-  
-  templateUrl: './user-login-form.component.html',
-  styleUrl: './user-login-form.component.scss'
-})
-export class UserLoginFormComponent {
+    selector: "app-user-login-form",
+    standalone: false,
 
+    templateUrl: "./user-login-form.component.html",
+    styleUrl: "./user-login-form.component.scss",
+})
+export class UserLoginFormComponent implements OnInit {
+    @input() userData = { Username: "", Password: "" };
+
+    constructor(
+        public fetchApiData: FetchApiDataService,
+        public dialogRef: MatDialogRef<UserLoginFormComponent>,
+        public snackBar: MatSnackBar
+    ) {}
+
+    ngOnInit(): void {}
+
+    loginUser(): void {
+        this.fetchApiData.userLogin(this.userData).subscribe(
+            (result) => {
+                this.dialogRef.close();
+                this.snackBar.open(result, "OK", {
+                    duration: 2000,
+                });
+            },
+            (result) => {
+                this.snackBar.open(result, "OK", {
+                    duration: 2000,
+                });
+            }
+        );
+    }
 }
