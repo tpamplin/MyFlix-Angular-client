1a1e08fdbcdd0c7946349409e5920b035f3cba0b
tpamplin <gartiman1@gmail.com>
2025-01-31 00:45:58 -0500

    create user registration component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    standalone: false,
+    styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'MyFlix-Angular-client';
+    title = "MyFlix-Angular-client";
+
+    constructor(public dialog: MatDialog) {}
+
+    openUserRegistrationDialog(): void {
+        this.dialog.open(UserRegistrationFormComponent, {
+            width: "280px",
+        });
+    }
 }
