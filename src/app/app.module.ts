a88f439171e9734240fbedb6cc4321c56d7ae9bf
tpamplin <gartiman1@gmail.com>
2025-01-31 00:05:43 -0500

    fix formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+
+// Import Angular material components
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+    providers: [provideAnimationsAsync()],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,89 +1,75 @@
-import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { HttpClient, HttpHeaders, HttpErrorResponse} from "@angular/common/http";
-import { Observable, throwError } from 'rxjs';
-import { map,  } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { catchError } from "rxjs/operators";
+import {
+    HttpClient,
+    HttpHeaders,
+    HttpErrorResponse,
+} from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { map } from "rxjs/operators";
 
-const apiUrl = "https://myflix-timpamplin-021f285e4632.herokuapp.com/"
+const apiUrl = "https://myflix-timpamplin-021f285e4632.herokuapp.com/";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root",
 })
-
 export class UserRegistrationService {
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) {}
 
     private getStoredToken(): any {
-        return localStorage.getItem('token');
+        return localStorage.getItem("token");
     }
 
     private getStoredUser(): any {
-        return localStorage.getItem('user')
+        return localStorage.getItem("user");
     }
 
     public getAllMovies(): Observable<any> {
         const token = this.getStoredToken();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + 'movies', 
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
-
+        };
+        return this.http
+            .get(apiUrl + "movies", options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public getMovie(Title: string): Observable<any> {
         const token = this.getStoredToken();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + 'movies/' + Title, 
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .get(apiUrl + "movies/" + Title, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public getDirector(name: string): Observable<any> {
         const token = this.getStoredToken();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + "directors/" + name, 
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .get(apiUrl + "directors/" + name, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public getGenre(name: string): Observable<any> {
         const token = this.getStoredToken();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + "genres/" + name,
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .get(apiUrl + "genres/" + name, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public getUser(): Observable<any> {
@@ -91,16 +77,12 @@ export class UserRegistrationService {
         const user = this.getStoredUser();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + "users/" + user.Username,
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .get(apiUrl + "users/" + user.Username, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public getUserFavorites(): Observable<any> {
@@ -108,16 +90,12 @@ export class UserRegistrationService {
         const user = this.getStoredUser();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.get(
-            apiUrl + "users/" + user.Username + "/favorites",
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .get(apiUrl + "users/" + user.Username + "/favorites", options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public addFavorite(movieID: string): Observable<any> {
@@ -125,39 +103,29 @@ export class UserRegistrationService {
         const user = this.getStoredUser();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.post(
-            apiUrl + "users/" + user.Username + "/favorites",
-            {"MovieID": movieID},
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
-
+        };
+        return this.http
+            .post(
+                apiUrl + "users/" + user.Username + "/favorites",
+                { MovieID: movieID },
+                options
+            )
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
-    public updateUser(userDetails:any): Observable<any> {
+    public updateUser(userDetails: any): Observable<any> {
         const token = this.getStoredToken();
         const user = this.getStoredUser();
         const options = {
-            headers: new HttpHeaders(
-            {
-                Authorization: 'Bearer ' + token,
-            }
-            )
-        }
-        return this.http.post(
-            apiUrl + "users/" + user.Username,
-            userDetails,
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
-
+            headers: new HttpHeaders({
+                Authorization: "Bearer " + token,
+            }),
+        };
+        return this.http
+            .post(apiUrl + "users/" + user.Username, userDetails, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public deleteUser(): Observable<any> {
@@ -165,16 +133,12 @@ export class UserRegistrationService {
         const user = this.getStoredUser();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-        }
-        return this.http.delete(
-            apiUrl + "users/" + user.Username, 
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+        };
+        return this.http
+            .delete(apiUrl + "users/" + user.Username, options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
     public deleteFavorite(movieID: string): Observable<any> {
@@ -182,45 +146,39 @@ export class UserRegistrationService {
         const user = this.getStoredUser();
         const options = {
             headers: new HttpHeaders({
-                Authorization: 'Bearer ' + token,
+                Authorization: "Bearer " + token,
             }),
-            body: {"MovieID": movieID}
-        }
-        return this.http.delete(
-            apiUrl + "users/" + user.Username + "/favorites", 
-            options
-        ).pipe(
-            map(this.extractResponseData),
-            catchError(this.handleError)
-        )
+            body: { MovieID: movieID },
+        };
+        return this.http
+            .delete(apiUrl + "users/" + user.Username + "/favorites", options)
+            .pipe(map(this.extractResponseData), catchError(this.handleError));
     }
 
-    public userLogin(userDetails:any): Observable<any> {
+    public userLogin(userDetails: any): Observable<any> {
         console.log(userDetails);
-        return this.http.post(
-            apiUrl + "login", 
-            userDetails
-        ).pipe(
-            catchError(this.handleError)
-        );
+        return this.http
+            .post(apiUrl + "login", userDetails)
+            .pipe(catchError(this.handleError));
     }
 
-    public userRegistration(userDetails:any): Observable<any> {
+    public userRegistration(userDetails: any): Observable<any> {
         console.log(userDetails);
-        return this.http.post(
-            apiUrl + "users", 
-            userDetails
-        ).pipe(
-            catchError(this.handleError)
-        );
+        return this.http
+            .post(apiUrl + "users", userDetails)
+            .pipe(catchError(this.handleError));
     }
-    
-    private handleError(error:HttpErrorResponse): any{
+
+    private handleError(error: HttpErrorResponse): any {
         if (error.error instanceof ErrorEvent) {
             console.error("some error occured: ", error.error.message);
         } else {
-            console.error(`Error Status code ${error.status}, error body is: ${error.error}` );
-            return throwError("Something bad happened; Please try again later.")
+            console.error(
+                `Error Status code ${error.status}, error body is: ${error.error}`
+            );
+            return throwError(
+                "Something bad happened; Please try again later."
+            );
         }
     }
 
@@ -230,5 +188,5 @@ export class UserRegistrationService {
     }
 }
 export class FetchApiDataService {
-    constructor() { }
+    constructor() {}
 }
