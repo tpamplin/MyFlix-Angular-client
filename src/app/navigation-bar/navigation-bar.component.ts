2750207a0b1b32cd91e6e08bd7abef6bb4e7508e
tpamplin <gartiman1@gmail.com>
2025-02-01 23:02:17 -0500

    create simple navigation logic

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-navigation-bar',
-  standalone: false,
-  
-  templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.scss'
-})
-export class NavigationBarComponent {
+    selector: "app-navigation-bar",
+    standalone: false,
 
+    templateUrl: "./navigation-bar.component.html",
+    styleUrl: "./navigation-bar.component.scss",
+})
+export class NavigationBarComponent implements OnInit {
+    constructor(
+        public router: Router,
+        public dialogRef: MatDialogRef<NavigationBarComponent>
+    ) {}
+
+    ngOnInit(): void {}
+
+    navigateMovies(): void {
+        this.router.navigate(["movies"]);
+    }
+
+    navigateProfile(): void {
+        this.router.navigate(["profile"]);
+    }
 }
